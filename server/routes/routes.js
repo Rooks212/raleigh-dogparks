f92e7e43c27227629f7738901ce041b61d64565d
Arthur Phillips <rooks212@gmail.com>
2020-04-28 22:03:25 -0400

    Cloudinary updated

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const request = require('request');
 const cloudinary = require('cloudinary');
-const CLOUDINARY_NAME1 = 'ddmeofkzuNew';
-const CLOUDINARY_KEY1 = '833162614866536New';
-const CLOUDINARY_SECRET1 = '7bChKulxDSPFbLgqSxIsSv3GcewNew';
+const CLOUDINARY_NAME1 = 'dkifvsfwx';
+const CLOUDINARY_KEY1 = '927547932953487';
+const CLOUDINARY_SECRET1 = 'KGA2uhKzU4buGGkZi6wFGyOOn24';
 const router = express.Router();
 
 cloudinary.config({ 
